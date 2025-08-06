9004c8d4639549632810ff6a90de7589d0038a30
Jagathesh1811 <jagatheshece1811@gmail.com>
2025-08-06 20:40:16 +0530

    Create design.v

---
+++ xor_gate/design.v
@@ -0,0 +1,16 @@
+
+
+entity xor_gate is
+port(
+  a: in std_logic;
+  b: in std_logic;
+  q: out std_logic);
+end xor_gate;
+
+architecture rtl of xor_gate is
+begin
+  process(a, b) is
+  begin
+    q <= a xor b;
+  end process;
+end rtl;
